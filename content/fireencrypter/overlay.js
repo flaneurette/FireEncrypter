696b0d34deeb2c208dce23cb27fec9d88c519d59
flaneurette <info@flaneurette.nl>
2018-11-05 15:19:00 +0100

    1

---
+++ content/fireencrypter/overlay.js
@@ -0,0 +1,3 @@
+function launchFireEncrypter() {
+	window.open("chrome://fireencrypter/content/popup.xul", "Fire Encrypter", "chrome,centerscreen,width=700,height=330,resizable,scrollbars=yes,status=1");
+}
